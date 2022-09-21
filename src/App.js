90491c48a0bd6c1a98e1ae0b1b180e86ca0be883
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-21 09:34:09 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Select from "react-select";
+import Creatable from 'react-select/creatable';
 
 function App() {
+
+const colorCreate = [
+  {label: "white", value: "white"},
+  {label: "Black", value: "Black"},
+  {label: "Blue", value: "Blue"},
+  {label: "Red", value: "Red"},
+  {label: "Green", value: "Green"},
+  {label: "Brown", value: "Brown"},
+  {label: "Purple", value: "Purple"},
+  {label: "Crimson", value: "Crimson"},
+] 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>selection labels</h2>
+      <Creatable
+       options = {colorCreate}
+       isMulti
+       onChange={(opt, meta) => console.log(opt, meta)}
+      />
     </div>
   );
 }
